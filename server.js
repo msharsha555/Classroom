5b0686c7bf6c3567d064aa93edd646606bfaf6d9
miryalaharshavardhan <miryalaharshavardhan4@gmail.com>
2017-11-04 12:45:41 +0530

    includes department pages and image crops

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express  = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
     expressValidator = require('express-validator'),
-    server = require('http').createServer(app)
-    io = require('socket.io').listen(server);
+    server = require('http').createServer(app);
 
 /*Set EJS template Engine*/
 app.set('views','./views');
@@ -47,18 +46,6 @@ dashboard.get(function(req,res,next){
 });
 
 var home = router.route('/');
-
-/*
-home.get(function(req,res,next){
-    connection.query('SELECT (SELECT Post_timestamp FROM Events ORDER BY Post_timestamp DESC Limit 3) As event_time,(SELECT Title FROM Events ORDER BY Post_timestamp DESC Limit 3) As events, (SELECT Title FROM News ORDER BY Post_timestamp DESC Limit 3) As news,(SELECT Post_timestamp FROM News ORDER BY Post_timestamp DESC Limit 3) As news_time', function (err, result) {
-        if(err){
-            throw err;
-        } else {
-            obj = {index: result};
-            res.render('index', obj);                
-        }        
-    });
-});*/
 var q;
 home.get(function(req,res,next){
     connection.query('SELECT Title,Post_timestamp FROM Events ORDER BY Post_timestamp DESC Limit 5', function (err, result1) {
@@ -92,6 +79,159 @@ department.get(function(req,res,next){
             obj = {department: result};
             res.render('department', obj);                
         }
+    });
+});
+
+var cse = router.route('/department/iitr_cse');
+var r,s,t;
+var str = 'iitr_cse';
+cse.get(function(req,res,next){
+    connection.query('SELECT * FROM Department WHERE ID =?',[str], function (err, result1) {
+        if(err){
+            throw err;
+        } else {
+            q = {index: result1};                
+        }        
+    });
+    connection.query('SELECT * FROM Dept_images WHERE d_ID =?',[str], function (err, result3) {
+        if(err){
+            throw err;
+        } else {
+            r = {index: result3};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_event natural join Events) WHERE d_ID =?',[str], function (err, result4) {
+        if(err){
+            throw err;
+        } else {
+            s = {index: result4};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_notice natural join News) WHERE d_ID =?',[str], function (err, result2) {
+        if(err){
+            throw err;
+        } else {
+            var obj ={};
+            obj.index = q.index;
+            obj.part2 = r.index;
+            obj.part3 = s.index;
+            obj.part4 = result2; 
+            res.render('cse', obj);         
+        }        
+    });
+});
+
+var ece = router.route('/department/iitr_ee');
+str = 'iitr_ee';
+ece.get(function(req,res,next){
+    connection.query('SELECT * FROM Department WHERE ID =?',[str], function (err, result1) {
+        if(err){
+            throw err;
+        } else {
+            q = {index: result1};                
+        }        
+    });
+    connection.query('SELECT * FROM Dept_images WHERE d_ID =?',[str], function (err, result3) {
+        if(err){
+            throw err;
+        } else {
+            r = {index: result3};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_event natural join Events) WHERE d_ID =?',[str], function (err, result4) {
+        if(err){
+            throw err;
+        } else {
+            s = {index: result4};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_notice natural join News) WHERE d_ID =?',[str], function (err, result2) {
+        if(err){
+            throw err;
+        } else {
+            var obj ={};
+            obj.index = q.index;
+            obj.part2 = r.index;
+            obj.part3 = s.index;
+            obj.part4 = result2; 
+            res.render('ece', obj);         
+        }        
+    });
+});
+
+var ce = router.route('/department/iitr_ce');
+str = 'iitr_ce';
+ce.get(function(req,res,next){
+    connection.query('SELECT * FROM Department WHERE ID =?',[str], function (err, result1) {
+        if(err){
+            throw err;
+        } else {
+            q = {index: result1};                
+        }        
+    });
+    connection.query('SELECT * FROM Dept_images WHERE d_ID =?',[str], function (err, result3) {
+        if(err){
+            throw err;
+        } else {
+            r = {index: result3};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_event natural join Events) WHERE d_ID =?',[str], function (err, result4) {
+        if(err){
+            throw err;
+        } else {
+            s = {index: result4};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_notice natural join News) WHERE d_ID =?',[str], function (err, result2) {
+        if(err){
+            throw err;
+        } else {
+            var obj ={};
+            obj.index = q.index;
+            obj.part2 = r.index;
+            obj.part3 = s.index;
+            obj.part4 = result2; 
+            res.render('ce', obj);         
+        }        
+    });
+});
+
+var civil = router.route('/department/iitr_civil');
+str = 'iitr_civil';
+civil.get(function(req,res,next){
+    connection.query('SELECT * FROM Department WHERE ID =?',[str], function (err, result1) {
+        if(err){
+            throw err;
+        } else {
+            q = {index: result1};                
+        }        
+    });
+    connection.query('SELECT * FROM Dept_images WHERE d_ID =?',[str], function (err, result3) {
+        if(err){
+            throw err;
+        } else {
+            r = {index: result3};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_event natural join Events) WHERE d_ID =?',[str], function (err, result4) {
+        if(err){
+            throw err;
+        } else {
+            s = {index: result4};                
+        }        
+    });
+    connection.query('SELECT * FROM (Dept_notice natural join News) WHERE d_ID =?',[str], function (err, result2) {
+        if(err){
+            throw err;
+        } else {
+            var obj ={};
+            obj.index = q.index;
+            obj.part2 = r.index;
+            obj.part3 = s.index;
+            obj.part4 = result2; 
+            res.render('civil', obj);         
+        }        
     });
 });
 
@@ -195,26 +335,6 @@ event.get(function(req,res,next){
 
 //now we need to apply our router here
 app.use(router);
-
-io.on('connection',function(socket){
-    console.log('a user connected');
-    socket.on('event_images',function(x){
-        var k = x;
-        connection.query('SELECT * FROM Event_images WHERE e_ID =?',[k],function(err,rows,fields){
-            if(err){
-                throw err;
-            } else {
-                /*obj = {images: result};
-                socket.emit('images_asked',k,obj);*/
-                for(var i in rows)
-                {
-                    socket.emit('images_asked',k,rows[i].Event_image_Url);
-                }
-            }
-        });
-    });
-});
-
 
 //start Server
 server.listen(process.env.PORT || 3000, function(){
